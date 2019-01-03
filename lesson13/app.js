19cf154998b125aa8a7b1e4c49e6f4bd2361e42b
volosgoto <volosgoto@yahoo.com>
2019-01-03 10:21:41 +0200

    arr methods

---
+++ lesson13/app.js
@@ -0,0 +1,71 @@
+// ARRAY FUNCTIONS
+
+// let arr = [1, 2, 3, 4, 5];
+
+// Methods
+// arr.pop() // del last elem
+// arr.push('hello') // add to last elem
+// arr.shift() // del first elem
+// arr.unshift('first') // add first elem
+// arr.length // returns last index +1  
+
+// arr2 =  [1,2,3]; // length == 3
+// arr2[10] = 10; // length == 11
+// console.log(arr2.length);
+
+// Перебор массива
+// for (let i = 0; i < arr.length; i++) {
+//   console.log(arr[i]);
+// }
+
+// arr.forEach((val, key)=>{
+//   console.log(`${key} : ${val}`);
+// });
+// 
+
+
+// testArr = ['hello', 'John', 5, 'Sara', 10];
+
+// for( let key in testArr){  // Выведет ключи
+//   console.log(key);
+// }
+
+// for (const key in testArr) {
+//   console.log(testArr[key]);
+// }
+
+// for( let key of testArr){  // Выведет значения
+//   console.log(key);
+// }
+
+// testArr = {
+//   name : 'Vova',
+//   status : 'Admin'
+// }
+
+// for( let key in testArr){  // Выведет значения
+//   console.log(testArr[key]);
+// }
+
+// STING TO ARRAY
+// let str = prompt();
+// let arr = [];
+// arr = str.split(',');
+// console.log (typeof arr);
+
+// ARRAY TO STRING
+// let arr = ['apple', 'pear', 'plum'];
+// let str = arr.join(',')
+// console.log(str);
+
+// ARRAY SORT
+// let arr = ['apple', 'zzz', 'pear', 'plum'];
+// let arr = [1,15,4];
+
+
+// function compareNum (a, b){
+//   return a-b;
+// }
+
+// let sort = arr.sort(compareNum);
+// console.log(sort);
