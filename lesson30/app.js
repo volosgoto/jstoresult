e6c1725513b2af9a626cf4b4a3079043b10e9db5
volosgoto <volosgoto@yahoo.com>
2019-01-11 16:05:10 +0200

    ajax curennsy exschenje

---
+++ lesson30/app.js
@@ -0,0 +1,11 @@
+let options = {
+	width : 768,
+	height : 1024,
+	background: 'red',
+	font: {
+		size : '16px',
+		color : 'blue'
+	}
+};
+
+console.log( JSON.parse(JSON.stringify(options)));
---
+++ lesson31/app.js
@@ -0,0 +1,31 @@
+// AJAX
+
+let inputRub = document.getElementById('rub');
+let inputUsd = document.getElementById('usd');
+
+// let btn = document.querySelector('button');
+
+
+inputRub.addEventListener('input', () =>{
+  let request = new XMLHttpRequest();
+  request.open('GET', 'current.json');
+  request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+  request.send();
+
+  // status
+  // statusText
+  // responseText
+  // response
+  // readyState
+
+  request.addEventListener('readystatechange', () => {
+   if(request.readyState === 4 && request.status == 200) {
+     let data = JSON.parse(request.response); 
+     inputUsd.value = inputRub.value / data.usd;
+     console.log(inputUsd.value);
+   } else {
+    inputUsd.value = "Что-то пошло не так";
+   }
+  });
+
+});
